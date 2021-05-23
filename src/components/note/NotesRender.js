4c74c6930443a2d32bbb2140695168f305b2d241
stupidgaming <loginfor357@gmail.com>
2021-05-23 15:59:11 +0530

    'update'

--- src/components/note/NotesRender.js
+++ src/components/note/NotesRender.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useAuth } from "../../context/AuthContext";
 import { useStateContext } from "../../context/StateContext";
